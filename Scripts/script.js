045916087a0bf63b8b53445346239760f481b907
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-09-17 15:57:24 +0200

    Added Calculator Data Population
    Added function to populate the calculator div with data pulled automatically from the JSON file.
    
    Refactored createShapeCard() function

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,13 @@
+//Global variable to pass JSOn data down to other functions
+let shapeArray = [];
 
 //Fetch the data from shapes.json and for each object run the function to create a html card containing the data
     function getShapeData() {
         fetch("../Data/shapes.json")
             .then((response) => response.json())
             .then((shapes) => {
+              //Save the array to a global variable for use in other functions
+              shapeArray = shapes;
                 for (let i = 0; i < shapes.length; i++) {
                     createShapeCard(shapes[i])
                 }
@@ -12,8 +16,7 @@
 
 //Function to loop through the json data and create an html card for each shape
 function createShapeCard(shape) {
-    const wrapper = document.getElementById("shape-list");
-    wrapper.innerHTML +=    `<div class="shape-card">
+    document.getElementById("shape-list").innerHTML +=    `<div class="shape-card" onclick=populateCalculator(${shape.id})>
                                 <img src="${shape.cardImage}">
                                 <h3> ${shape.shapeName}</h3>
                                 
@@ -35,3 +38,13 @@ function hidePhoneMenu(){
 var menu = document.getElementById("phone-menu");
 menu.style.display = "none";
 };
+
+//Function to onclick populate calculator with data from corresponding shape object
+function populateCalculator (shapeId) {
+    document.getElementById("calc-area").innerHTML = 
+                          `
+                          <img src="${shapeArray[shapeId].calcImage}" alt="Image of ${shapeArray[shapeId].shapeName}">
+                          <br>
+                          <p>${shapeArray[shapeId].shapeName}</p>
+                          `
+}
