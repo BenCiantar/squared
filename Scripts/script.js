3073438a92bc3a74f0e2b50e1746f7264494118a
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-10-04 21:20:17 +0200

    Small layout change
    Made slight adjustment to the calculator input margins

--- Scripts/script.js
+++ Scripts/script.js
@@ -66,20 +66,6 @@ function populateCalculator (shapeId) {
                           `
 }
 
-
-//Phone Navigation Menu
-
-//Toggle phone menu display when clicking menu icon
-// function togglePhoneMenu() {
-//   const menu = document.getElementById("phone-menu");
-//   if (menu.style.display === "none") {
-//     menu.style.display = "block";
-//   } else {
-//     menu.style.display = "none";
-//   }
-// }
-
-
 //Hide phone menu and reset icon on screen resize
 function hidePhoneMenu(){
   let menu = document.getElementById("phone-menu");
