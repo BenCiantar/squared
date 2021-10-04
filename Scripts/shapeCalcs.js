bd25084c2c66b4938065427f4654267eba93212d
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-10-04 13:21:07 +0200

    Added parallelogram formula

--- Scripts/shapeCalcs.js
+++ Scripts/shapeCalcs.js
@@ -557,3 +557,85 @@ function calculatorNonagon() {
     }
     return false; //Prevent the form from submitting if enter is pressed
 }
+
+
+//PARALLELOGRAM
+
+function calculatorParallelogram() {
+    let n1 = parseInt(document.getElementById('n1').value, 10);
+    let num1 = n1;
+
+    let unit1 = document.getElementById('unit1').value;
+
+    //Convert input 1 to mm
+    if (unit1 == "mm") {
+        n1 = n1;
+    } else if (unit1 == "cm") {
+        n1 = n1 * 10;
+    } else if (unit1 == "m") {
+        n1 = n1 * 1000;
+    } else if (unit1 == "km") {
+        n1 = n1 * 1000000;
+    }
+
+    let n2 = parseInt(document.getElementById('n2').value, 10);
+    let num2 = n2;
+
+    let unit2 = document.getElementById('unit2').value;
+
+    //Convert unit 2 to mm
+    if (unit2 == "mm") {
+        n2 = n2;
+    } else if (unit2 == "cm") {
+        n2 = n2 * 10;
+    } else if (unit2 == "m") {
+        n2 = n2 * 1000;
+    } else if (unit2 == "km") {
+        n2 = n2 * 1000000;
+    }
+
+    let outUnit1 = document.getElementById('outUnit1').value;
+
+    if (n1 > 0 && n2 > 0) {
+        //Convert from mm to desired output unit and output perimeter
+        if (outUnit1 == "mm") {
+            document.getElementById('perimeter').innerHTML = (num1 + num1 + num2 + num2).toFixed(1);
+        } else if (outUnit1 == "cm") {
+            num1 = n1 / 10;
+            num2 = n2 / 10;
+            document.getElementById('perimeter').innerHTML = (num1 + num1 + num2 + num2).toFixed(1);
+        } else if (outUnit1 == "m") {
+            num1 = n1 / 1000;
+            num2 = n2 / 1000;
+            document.getElementById('perimeter').innerHTML = (num1 + num1 + num2 + num2).toFixed(1);
+        } else if (outUnit1 == "km") {
+            num1 = n1 / 1000000;
+            num2 = n2 / 1000000;
+            document.getElementById('perimeter').innerHTML = (num1 + num1 + num2 + num2).toFixed(1);
+        } else {
+            console.log("Please enter two valid numbers");
+        }
+    }
+
+    let outUnit2 = document.getElementById('outUnit2').value;
+
+    if (n1 > 0 && n2 > 0) {
+        //Convert from mm to desired output unit and output area  
+        if (outUnit2 == "mm") {
+            document.getElementById('area').innerHTML = (n1 * n2).toFixed(1);
+        } else if (outUnit2 == "cm") {
+            n1 = n1 / 10;
+            n2 = n2 / 10;
+            document.getElementById('area').innerHTML = (n1 * n2).toFixed(1);
+        } else if (outUnit2 == "m") {
+            n1 = n1 / 1000;
+            n2 = n2 / 1000;
+            document.getElementById('area').innerHTML = (n1 * n2).toFixed(1);
+        } else if (outUnit2 == "km") {
+            n1 = n1 / 1000000;
+            n2 = n2 / 1000000;
+            document.getElementById('area').innerHTML = (n1 * n2).toFixed(1);
+        }
+    }
+    return false;
+} 
